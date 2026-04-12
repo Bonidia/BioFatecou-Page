8cb3f19bf0e1318413a590f3cec061d9ef750d60
Bonidia <robservidor@gmail.com>
2026-04-12 16:51:50 -0300

    news

--- cursos/config.js
+++ cursos/config.js
@@ -1,3 +1,3 @@
 window.APP_CONFIG = {
-  API_URL: "https://script.google.com/macros/s/AKfycbyjb3xKg5lAcDvjka4vs991bnT0VJoYR5ZmkX5tb5ZBp-Wwr_PtGCkEddByAIRMy23X/exec"
+  API_URL: "https://script.google.com/macros/s/AKfycbxnYKfL5az3wVWwuIw1Ky2pmPch1DTvjenfvAYxuhEYlOB_YAsk-DO28aP3pJiEGDQc/exec"
 };
